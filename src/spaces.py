261ec4947e379531981c4ef443b84ef9d5423dd4
Grigor Bezirganyan <grigor.bezirganyan98@gmail.com>
2021-05-12 21:10:43 +0200

    Fixed bug regarding unindented methods in spaces.py

--- src/spaces.py
+++ src/spaces.py
@@ -32,18 +32,16 @@ class EpiNetworkGrid(NetworkGrid):
             print(e)
             raise e
 
+    def is_cell_empty(self, node_id: int) -> bool:
+        """ Returns a bool of the contents of a cell. """
+        building = self.G.nodes[node_id]["building"]
+        return not sum(list(map(len, list(building.apartments.values()))))
 
-def is_cell_empty(self, node_id: int) -> bool:
-    """ Returns a bool of the contents of a cell. """
-    building = self.G.nodes[node_id]["building"]
-    return not sum(list(map(len, list(building.apartments.values()))))
 
-
-def iter_cell_list_contents(self, cell_list: List[int]) -> List[GridContent]:
-    list_of_lists = [
-        list(self.G.nodes[node_id]["building"].apartments.values())
-        for node_id in cell_list
-        if not self.is_cell_empty(node_id)
-    ]
-    return [item for sublist in list_of_lists for ap in sublist for item in ap]
-
+    def iter_cell_list_contents(self, cell_list: List[int]) -> List[GridContent]:
+        list_of_lists = [
+            list(self.G.nodes[node_id]["building"].apartments.values())
+            for node_id in cell_list
+            if not self.is_cell_empty(node_id)
+        ]
+        return [item for sublist in list_of_lists for ap in sublist for item in ap]
